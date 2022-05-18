c5367831fffa45eeada02db2a9fecc49075f5df9
Madhuparna Dutta <madhuparna.dutta666@gmail.com>
2022-05-18 16:18:50 +0530

    mode

---
+++ sql_trendytech/Mode_in_sql.sql
@@ -0,0 +1,30 @@
+-- mode is most frequent value in the array
+
+use customer;
+
+create table mode(id int);
+
+insert into mode(id)values(1);
+insert into mode(id)values(2);
+insert into mode(id)values(2);
+
+insert into mode(id)values(3);
+insert into mode(id)values(3);
+insert into mode(id)values(3);
+insert into mode(id)values(3);
+
+insert into mode(id)values(4);
+insert into mode(id)values(5);
+
+select * from mode;
+
+
+
+
+
+
+
+
+
+
+
