d6088fd2b1cf04da6095977b7894571a074610c2
Madhuparna Dutta <madhuparna.dutta666@gmail.com>
2022-05-18 16:28:16 +0530

    Mode

--- sql_trendytech/Mode_in_sql.sql
+++ sql_trendytech/Mode_in_sql.sql
@@ -20,6 +20,17 @@ select * from mode;
 
 
 
+-- will return the frequency of each unique number
+select id, count(*) as freq from mode group by id;
+
+-- Method CTE(common table expression)
+
+with freq_cte as (
+select id, count(*) as freq from mode group by id)
+
+select * from freq_cte
+where freq = 
+	(select max(freq) from freq_cte);
 
 
 
