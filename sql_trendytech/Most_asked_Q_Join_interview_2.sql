1305be535133f484dc88c0703860f540ad8c735b
Madhuparna Dutta <madhuparna.dutta666@gmail.com>
2022-05-18 14:21:37 +0530

    sql

--- sql_trendytech/Most_asked_Q_Join_interview_2.sql
+++ sql_trendytech/Most_asked_Q_Join_interview_2.sql
@@ -1,6 +1,7 @@
+use customer;
 create table t1 (id1 int);
 create table t2 (id2 int);
-
+drop table t1;
 insert into t1 values(1);
 insert into t1 values(1);
 insert into t1 values(2);
@@ -12,7 +13,8 @@ insert into t2 values(3);
 
 select * from t1;
 select * from t2;
-
+-- will return the matching records (in t1 if we have (1,1) and in t2 we have (1,1,1) 
+-- then in output will get 6 number of 1s (2x3 = 6) )
 select * from t1
 inner join t2 on t1.id1 = t2.id2;
 
