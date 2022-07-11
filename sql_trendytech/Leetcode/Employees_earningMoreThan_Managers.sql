b5a4c5bd6772917ff58b9278eb41b401e8e46bc2
Madhuparna Dutta <madhuparna.dutta666@gmail.com>
2022-07-11 12:05:37 +0530

    with/withoutJOIN

--- sql_trendytech/Leetcode/Employees_earningMoreThan_Managers.sql
+++ sql_trendytech/Leetcode/Employees_earningMoreThan_Managers.sql
@@ -6,3 +6,10 @@ SELECT e1.NAME AS Employee
 FROM Employee e1
 join Employee e2 
 ON e1.managerId = e2.id AND e1.salary > e2.salary;
+
+
+/* without join */
+
+select e.Name as Employee 
+from Employee e, Employee m 
+where e.ManagerId = m.Id and e.Salary > m.Salary;
